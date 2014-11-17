94e699d64472b145eda2ab28ae3e3bf8ae2c4195
Raynos <raynos2@gmail.com>
2014-11-17 01:12:32 -0800

    Get gauge() to write to the socket directly.
    For some reason gauge() breaks in production when using the
        packet queue. Dont know why.
    
    We'll skip the packet queue for the gauge and see if that helps.

--- test/socket.js
+++ test/socket.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var dgram = require('dgram');
 
 var EphemeralSocket = require('../lib/EphemeralSocket.js');
+var StatsDClient = require('../lib/statsd-client.js');
 
 var PORT = 8125;
 
@@ -115,6 +116,51 @@ test('socket will close and timeout', function t(assert) {
         function expectClosed() {
             assert.equal(sock._socket, undefined);
 
+            server.close();
+            assert.end();
+        }
+    });
+});
+
+test('client.gauge()', function t(assert) {
+    var server = UDPServer({ port: PORT }, function onBound() {
+        var sock = new StatsDClient({
+            host: 'localhost',
+            port: PORT,
+            packetQueue: { flush: 10 }
+        });
+
+        server.once('message', onMessage);
+        sock.gauge('hello', 10);
+
+        function onMessage(msg) {
+            var str = String(msg);
+            assert.equal(str, 'hello:10|g');
+
+            sock.close();
+            server.close();
+            assert.end();
+        }
+    });
+});
+
+
+test('client.counter()', function t(assert) {
+    var server = UDPServer({ port: PORT }, function onBound() {
+        var sock = new StatsDClient({
+            host: 'localhost',
+            port: PORT,
+            packetQueue: { flush: 10 }
+        });
+
+        server.once('message', onMessage);
+        sock.counter('hello', 10);
+
+        function onMessage(msg) {
+            var str = String(msg);
+            assert.equal(str, 'hello:10|c\n');
+
+            sock.close();
             server.close();
             assert.end();
         }
